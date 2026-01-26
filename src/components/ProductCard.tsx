f1cc80a79033b014a2a2cd7d0b50e9c3c185183f
vivek <vivekgpt107@gmail.com>
2026-01-26 22:05:24 +0530

    added the number memory test content

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,6 +3,7 @@ import type { CardProps } from "@heroui/react";
 
 import { Button, Card, Image, CardBody } from "@heroui/react";
 import {
+    BrainIcon,
     ExternalLink,
     Icon,
     Keyboard,
@@ -15,7 +16,7 @@ const ProductCard = (
     props: CardProps & {
         heading: string;
         children: ReactNode;
-        icon?: "zap" | "keyboard";
+        icon?: "zap" | "keyboard" | "brain";
         href?: string;
     },
 ) => {
@@ -25,6 +26,35 @@ const ProductCard = (
         children: undefined,
         icon: undefined,
     };
+    let iconComponent: React.ReactNode = null;
+
+    switch (props.icon) {
+        case "keyboard":
+            iconComponent = (
+                <Keyboard
+                    size={80}
+                    className="group-hover:text-primary mx-2 transition-colors "
+                />
+            );
+            break;
+        case "zap":
+            iconComponent = (
+                <Zap
+                    size={80}
+                    className="group-hover:text-warning mx-2 transition-colors "
+                />
+            );
+            break;
+        case "brain":
+            iconComponent = (
+                <BrainIcon
+                    size={80}
+                    className="group-hover:text-success transition-colors mx-2"
+                />
+            );
+            break;
+    }
+
     return (
         <Card
             className="w-full max-w-[520px] not-prose not-content group"
@@ -45,19 +75,7 @@ const ProductCard = (
                 />
             </Button>
             <CardBody className="flex flex-col sm:flex-row flex-wrap p-0 sm:flex-nowrap ">
-                <div className="self-center mt-2">
-                    {props.icon === "keyboard" ? (
-                        <Keyboard
-                            size={80}
-                            className="group-hover:text-primary mx-2"
-                        />
-                    ) : (
-                        <Zap
-                            size={80}
-                            className="group-hover:text-warning mx-2"
-                        />
-                    )}
-                </div>
+                <div className="self-center mt-2">{iconComponent}</div>
                 <div className="px-4 py-5">
                     <h3 className="text-large font-medium">{props.heading}</h3>
                     <div className="text-small text-default-400 flex flex-col gap-3 pt-2">
